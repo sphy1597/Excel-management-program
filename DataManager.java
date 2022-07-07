f6c59dc4b0d951ace6fb4b4c425fbf9456783119
sphy1597 <101171867+sphy1597@users.noreply.github.com>
2022-07-07 18:09:57 +0900

    Version 4 ( delet w, r )
    Write and Read class >> DataManager method

--- DataManager.java
+++ DataManager.java
@@ -1,6 +1,8 @@
+package main;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -9,149 +11,139 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class DataManager {
-	
-	ArrayList<String> colname = new ArrayList<String>();
-	ArrayList<Dataform> datas = new ArrayList<Dataform>();
-	ArrayList<String> values = new ArrayList<String>();
-	
-	
+
+	ArrayList<String> datas = new ArrayList<String>(); // 각 행에대한 내용을 저장
+
 	File file;
-	
+
 	FileInputStream fis;
 	XSSFWorkbook workbook;
 	XSSFSheet sheet;
-	
-	DataManager(){
-		
+
+	int cells;
+	int rows;
+
+	DataManager() { // 엑셀 파일을 객체로 생성
+
 		try {
-			
-    		file = new File("data.xlsx");
+
+			file = new File("data.xlsx");
 			fis = new FileInputStream(file);
-			workbook = new XSSFWorkbook(fis);		
+			workbook = new XSSFWorkbook(fis);
 			sheet = workbook.getSheetAt(0);
-			
-    	}catch(Exception e) {
-    		e.getStackTrace();
-    	}
+
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
 	}
-	
-	
-	public void ReadData() {
-		
+
+	public void ReadData() { // 엑셀에서 값을 읽어옴
+
 		datas.clear();
-		
+
 		int rowindex = 0;
 		int colindex = 0;
 
-		//행수를 가져옴
-		int rows = sheet.getPhysicalNumberOfRows();
-		
-		//칼럼들을 가져옴
-		XSSFRow namerow = sheet.getRow(1);		
-		int namecells = namerow.getPhysicalNumberOfCells();
-		
-		//반복문으로 리스트에 저장 
-		for(int i=0;i<namecells;i++) {
+		// 행수를 가져옴
+		rows = sheet.getPhysicalNumberOfRows();
 
-			XSSFCell namecell = namerow.getCell(colindex);
-			String namevalue = "";
-			
-			if(namecell==null) {
-				continue;
-			}else {
-				switch(namecell.getCellType()) {
-    			case FORMULA:
-    				namevalue = namecell.getCellFormula();
-    				break;
-    			case STRING:
-    				namevalue = namecell.getStringCellValue()+"";
-    				break;
-    			case NUMERIC:
-    				namevalue = namecell.getNumericCellValue()+"";
-    				break;
-    			case BLANK:
-    				namevalue = namecell.getBooleanCellValue()+"";
-    				break;
-    			case ERROR:
-    				namevalue = namecell.getErrorCellValue()+"";
-    				break;
-    			}
-				
-				colname.add(namevalue); // 컬럼이름들 리스트에 추가
-			}
-			
-		}//칼럼 가져오는 반복문
-		
-		//가로로 한줄씩 쭉 가져옴 
-		
-		
-		for(rowindex=1;rowindex<rows;rowindex++) {
-			
-			//행읽기 
+		// 가로로 한줄씩 쭉 가져옴
+		for (rowindex = 0; rowindex < rows; rowindex++) {
+
+			// 행읽기
 			XSSFRow row = sheet.getRow(rowindex);
 
-			//셀의 수
-			int cells = row.getPhysicalNumberOfCells();
-			
-			//한칸씩 가져옴 
-			for(colindex = 0 ; colindex < cells; colindex++) {
-				
+			// 셀의 수
+			cells = row.getPhysicalNumberOfCells();
+
+			// 한칸씩 가져옴
+			for (colindex = 0; colindex < cells; colindex++) {
+
 				XSSFCell cell = row.getCell(colindex);
 				String value = "";
-				   				
-				
-				if(cell==null) {
-					continue;
-				}else {
-					
-					//가져온 셀의 종류에 따라 값을 읽어와 저장
-	    			switch(cell.getCellType()) {
-	    			case FORMULA:
-	    				value = cell.getCellFormula();
-	    				break;
-	    			case STRING:
-	    				value = cell.getStringCellValue()+"";
-	    				break;
-	    			case NUMERIC:
-	    				value = cell.getNumericCellValue()+"";
-	    				break;
-	    			case BLANK:
-	    				value = cell.getBooleanCellValue()+"";
-	    				break;
-	    			case ERROR:
-	    				value = cell.getErrorCellValue()+"";
-	    				break;
-	    			}
-	    			
-				}
-				
-				values.add(value);				
+
+				if (cell == null) {
+					value = "null";
+				} else {
+
+					// 가져온 셀의 종류에 따라 값을 읽어와 저장
+					switch (cell.getCellType()) {
+					case FORMULA:
+						value = cell.getCellFormula();
+						break;
+					case STRING:
+						value = cell.getStringCellValue() + "";
+						break;
+					case NUMERIC:
+						value = cell.getNumericCellValue() + "";
+						break;
+					case BLANK:
+						value = cell.getBooleanCellValue() + "";
+						break;
+					case ERROR:
+						value = cell.getErrorCellValue() + "";
+						break;
+					}// switch
+
+				} // if-else
+
+				datas.add(value);
+
+			} // for2
+
+		} // for1
+	}
+
+	public void PrintData() { // 읽어온 데이터를 프린트
+		System.out.println(rows);
+		System.out.println(cells);
+		System.out.println(datas.get(7));
+		for (int i = 0; i < rows * cells; i++) {
+			System.out.print(datas.get(i) + "  ");
+			if (i % cells == cells - 1) {
+				System.out.println("");
 			}
-			
-				Dataform data = new Dataform(values.get(0), values.get(1), values.get(2));
-				values.clear();
-				datas.add(data);
-				
 		}
+
 	}
-	
-	public void PrintData() {
-		for(int i = 0 ; i < datas.size() ; i ++) {
-			System.out.println(i+1+"번");
-			System.out.println(datas.get(i).id);
-			System.out.println(datas.get(i).name);
-			System.out.println(datas.get(i).num);
-			System.out.println("==============");			
-		}	
+
+	public void WriteData() { // 엑셀 파일에 값을 입력
+
+		XSSFRow row2 = sheet.createRow(0); // 행 >> 몇번째 줄
+		XSSFCell cell2 = row2.createCell(0); // 셀 >> 몇번쨰 칸
+		int index = 0; // 리스트에서의 인덱스
+
+		for (int i = 0; i < rows; i++) {
+			row2 = sheet.createRow(i);
+			for (int j = 0; j < cells; j++) {
+				cell2 = row2.createCell(j);
+				cell2.setCellValue(datas.get(index));
+				index++;
+			}
+
+		}
+
+		try {
+
+			FileOutputStream fOutStream = new FileOutputStream("data.xlsx");
+			workbook.write(fOutStream);
+			fOutStream.close();
+			System.out.println("엑셀파일생성성공");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("엑셀파일생성실패");
+			System.out.println("DataManager >> WriteData");
+		}
+
 	}
-	
-	public void WriteData() {
-						
-		XSSFRow row = sheet.getRow(5);		
-		XSSFCell cell = row.getCell(0);
-		cell.setCellValue("aaa");
-		
+
+	// x행 줄, y가 셀 칸
+	public void changevalue(int _x, int _y, String _value) {
+
+		int index = (_x * cells) - (cells - _y);
+		datas.set(index, _value);
+
 	}
-	
-	
+
 }
---
+++ Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+
+	static DataManager dm;
+
+	public static void main(String[] args) {
+
+		dm = new DataManager();
+		dm.ReadData();
+		dm.PrintData();
+
+	}
+
+}
